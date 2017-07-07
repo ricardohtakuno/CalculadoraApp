f8f79b2d9081a7e3e55e45bc2db0fe6f1706cf51
Ricardo <takunoricardo@gmail.com>
2017-07-06 22:29:47 -0300

    Desenvolvimento da calculadora

---
+++ App4.teste/App4.teste/App4.teste/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App4.teste
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new App4.teste.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
