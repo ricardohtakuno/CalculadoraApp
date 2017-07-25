01210108c60c3cf0029cee99a2527f9adabb61f8
Ricardo <takunoricardo@gmail.com>
2017-07-24 22:34:46 -0300

    aplicativo

--- App4.teste/App4.teste/App4.teste/MainPage.xaml.cs
+++ App4.teste/App4.teste/App4.teste/MainPage.xaml.cs
@@ -13,5 +13,49 @@ namespace App4.teste
 		{
 			InitializeComponent();
 		}
+        void btnCalcular(object sender, System.EventArgs e)
+        {
+            double ValorTotal = double.Parse(txtValorTotal.Text);
+            int NumeroParcelas = Convert.ToInt32(txtNumeroParcelas.Text);
+            double Desconto = double.Parse(txtDesconto.Text);
+            double Acrescimo = double.Parse(txtAcrescimo.Text);
+            double Porcentagem = double.Parse(txtPorcentagem.Text);
+            double ValorPagar = double.Parse(txtValorPagar.Text);
+            double ValorParcelas = double.Parse(txtValorParcelas.Text);
+
+            double ValorPorcentagem;
+            if(txtPorcentagem.Text.Length >= 1)
+            {
+                ValorPorcentagem = double.Parse(txtPorcentagem.Text);
+            }
+            else
+            {
+                ValorPorcentagem = 0;
+            }
+            double porcentagem = (ValorTotal * ValorPorcentagem) / 100;
+
+            if(txtDesconto.Text == "")
+            {
+                ValorPagar = ValorTotal - porcentagem;
+                ValorParcelas = ValorPagar / NumeroParcelas;
+                txtValorPagar.Text = ValorPagar.ToString();
+                txtValorParcelas.Text = ValorParcelas.ToString();
+                //return;
+            }
+
+            if(txtAcrescimo.Text == "")
+            {
+                ValorPagar = ValorTotal + porcentagem;
+                ValorParcelas = ValorPagar / NumeroParcelas;
+                txtValorPagar.Text = ValorPagar.ToString();
+                txtValorParcelas.Text = ValorParcelas.ToString();
+                //return;
+            }
+
+            ValorParcelas = ValorTotal / NumeroParcelas;
+
+            txtValorPagar.Text = ValorTotal.ToString();
+            txtValorParcelas.Text = ValorParcelas.ToString();
+        }
 	}
 }
